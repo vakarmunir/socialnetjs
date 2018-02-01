e2d31984e2b1b30e1eaf8be869a8704d67f18789
Waqar Muneer <waqarmuneer@gmail.com>
2018-02-01 11:48:06 +0500

    backend API - Activity model

---
+++ backend/api/activity/activity.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const ActivitySchema = mongoose.Schema({
+    published : {type:Date, Default:Date.now},
+    actor : {
+        objectType : {type: String},
+        _id : {type: mongoose.Schema.Types.ObjectId} //userId
+    },
+    verb: {type:String , Default:"post"}, //share , like
+    object : {
+        objectType : {type: String},
+        _id : {type: mongoose.Schema.Types.ObjectId} //postId (vid,aud,img)
+    },
+    target : {
+        objectType : {type: String},
+        _id : {type: mongoose.Schema.Types.ObjectId} //postId (in album)
+    },
+});
+
+const Activity = mongoose.model("Activity",ActivitySchema);
+module.exports = Activity;
