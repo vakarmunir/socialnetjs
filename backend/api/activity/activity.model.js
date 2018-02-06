6f5b17d419105adb3f988b9da8a2317ec4f779ea
Waqar Muneer <waqarmuneer@gmail.com>
2018-02-06 11:03:41 +0500

    activity model

--- backend/api/activity/activity.model.js
+++ backend/api/activity/activity.model.js
@@ -4,16 +4,16 @@ const ActivitySchema = mongoose.Schema({
     published : {type:Date, default:Date.now},
     actor : {
         objectType : {type: String, default: 'user'}, //user, page, group
-        _id : {type: mongoose.Schema.Types.ObjectId} //userId
+        _id : {type: mongoose.Schema.Types.ObjectId , ref: 'Usser'} //userId
     },
     verb: {type:String , default:"posted"}, //shared , liked
     object : {
         objectType : {type: String, default: 'post'},
-        _id : {type: mongoose.Schema.Types.ObjectId} //postId (vid,aud,img)
+        _id : {type: mongoose.Schema.Types.ObjectId, ref: 'Post'} //postId (vid,aud,img)
     },
     target : {
         objectType : {type: String},
-        _id : {type: mongoose.Schema.Types.ObjectId} //postId (in album)
+        _id : {type: mongoose.Schema.Types.ObjectId , ref: 'Post'} //postId (in album)
     },
 });
 
